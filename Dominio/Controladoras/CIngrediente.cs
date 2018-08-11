b7223eb7d6c1acdb8196a87c67e25959d0a7821d
ShyGuyMD <matias.pereyra1@gmail.com>
2018-08-11 05:02:17 -0300

    Finalizadas las altas, acomodado el código.

--- Dominio/Controladoras/CIngrediente.cs
+++ Dominio/Controladoras/CIngrediente.cs
@@ -33,11 +33,90 @@ namespace Dominio.Controladoras
 
         public bool AltaIngrediente(string pCodigo, string pDescripcion, decimal pCosto)
         {
-            throw new NotImplementedException();
+            bool ret = false;
+
+            if (ValidarData(pCodigo, pDescripcion, pCosto))
+            {
+                Ingrediente i = Buscar(pCodigo);
+                if (i == null)
+                {
+                    i = new Ingrediente()
+                    {
+                        Codigo = pCodigo,
+                        Descripcion = pDescripcion,
+                        Costo = pCosto
+                    };
+                    _Ingredientes.Add(i);
+                    ret = true;
+                }
+                else if (i != null && !i.Activo)
+                {
+                    i.Descripcion = pDescripcion;
+                    i.Costo = pCosto;
+                    i.Activo = true;
+                    ret = true;
+                }
+            }
+
+            return ret;
         }
-        public bool BajaIngrediente(int pIdIngrediente)
+
+        public IngredientesPorMenu ArmarObjetoIngrediente(Ingrediente i, int cantidad)
         {
-            throw new NotImplementedException();
+            IngredientesPorMenu ret = new IngredientesPorMenu()
+            {
+                Ingrediente = i,
+                Cantidad = cantidad
+            };
+
+            return ret;
+        }
+
+        public bool BajaIngrediente(string pCodigo)
+        {
+            bool ret = false;
+            if (pCodigo != null)
+            {
+                Ingrediente i = Buscar(pCodigo);
+                if (i != null)
+                {
+                    i.Activo = false;
+                    ret = true;
+                }
+            }
+
+            return ret;
+        }
+
+        public Ingrediente Buscar(string pCodigo)
+        {
+            Ingrediente ret = null;
+            int contador = 0;
+
+            while (contador < _Ingredientes.Count && ret == null)
+            {
+                if (_Ingredientes[contador].Codigo == pCodigo)
+                {
+                    ret = _Ingredientes[contador];
+                }
+                contador++;
+            }
+
+            return ret;
+        }
+
+        public Ingrediente BuscarActivo(string pCodigo)
+        {
+            Ingrediente ret = Buscar(pCodigo);
+            if (!ret.Activo)
+                ret = null;
+
+            return ret;
+        }
+
+        public bool ValidarData(string pCodigo, string pDescripcion, decimal pCosto)
+        {
+            return (pCodigo != "" && pDescripcion != "" && pCosto > 0);
         }
     }
 }
