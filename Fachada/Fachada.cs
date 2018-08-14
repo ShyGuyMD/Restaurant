ee4ffce3a31a3df6eecaa3fb83d43b06b03dab1f
ShyGuyMD <ShyGuyMD@users.noreply.github.com>
2018-08-14 17:33:10 -0300

    O SHIT WADDUP

--- Fachada/Fachada.cs
+++ Fachada/Fachada.cs
@@ -294,7 +294,47 @@ namespace Aplicacion
         }
         public void CargarDatosDePrueba()
         {
-
+            Encryption e = new Encryption();
+            
+            //AltaAdmin(string pUsername, string pPassword, string pRol)
+            AltaAdmin("admin", e.EncryptToString("admin"), "Administrador");
+            
+            //AltaChef(string pUsername, string pPassword, string pRol, string pNumDoc, string pTipoDoc, string pNombre, string pApellido, decimal pSueldo)
+            AltaChef("angel@mail.com", e.EncryptToString("angel"), "Chef", "1.111.111-1", "Cedula", "Warren", "Worthington", 30000);
+            AltaChef("beast@mail.com", e.EncryptToString("beast"), "Chef", "2.222.222-2", "Pasaporte", "Hank", "McCoy", 25000);
+            AltaChef("cyclops@mail.com", e.EncryptToString("cyclops"), "Chef", "3.333.333-3", "Otro", "Scott", "Summers", 10000);
+            AltaChef("iceman@mail.com", e.EncryptToString("iceman"), "Chef", "4.444.444-4", "Cedula", "Bobby", "Drake", 40000);
+            AltaChef("marvelgirl@mail.com", e.EncryptToString("marvelgirl"), "Chef", "5.555.555-5", "Pasaporte", "Jean", "Grey", 50000);
+            AltaChef("professorx@mail.com", e.EncryptToString("professorx"), "Chef", "6.666.666-6", "Otro", "Charles", "Xavier", 35000);
+            AltaChef("magneto@mail.com", e.EncryptToString("magneto"), "Chef", "7.777.777-7", "Cedula", "Max", "Eisenhardt", 15000);
+            AltaChef("storm@mail.com", e.EncryptToString("storm"), "Chef", "8.888.888-8", "Pasaporte", "Ororo", "Munroe", 35000);
+            AltaChef("colossus@mail.com", e.EncryptToString("colossus"), "Chef", "9.999.999-9", "Otro", "Piotr", "Rasputin", 45000);
+            
+            //AltaMesa(int pNumero, int pCapacidad, string pUbicacion)
+            AltaMesa(1, 2, "Pareja - Ala Norte");
+            AltaMesa(2, 5, "Familiar - Ala Norte");
+            AltaMesa(3, 12, "Reunion - Ala Norte");
+            AltaMesa(4, 2, "Pareja - Ala Este");
+            AltaMesa(5, 5, "Familiar - Ala Este");
+            AltaMesa(6, 12, "Reunion - Ala Este");
+            AltaMesa(7, 2, "Pareja - Ala Sur");
+            AltaMesa(8, 5, "Familiar - Ala Sur");
+            AltaMesa(9, 12, "Reunion - Ala Sur");
+            AltaMesa(10, 2, "Pareja - Ala Oeste");
+            AltaMesa(11, 5, "Familiar - Ala Oeste");
+            AltaMesa(12, 12, "Reunion - Ala Oeste");
+            
+            //AltaMenuPreelaborado(string pProveedor, decimal pCosto, string pDesc)
+            AltaMenuPreelaborado("Mc Dudels", 120, "Hamburguesa con queso");
+            AltaMenuPreelaborado("Mc Dudels", 220, "Hamburguesa doble con queso");
+            AltaMenuPreelaborado("Pizza Queen", 300, "Pizzeta familiar");
+            AltaMenuPreelaborado("Pizza Queen", 400, "Pizzeta familiar con gustos");
+            AltaMenuPreelaborado("Mc Dudels", 85, "Papas fritas");
+            
+            // Los Ingredientes de Código A01 hasta A10 se cargan desde el archivo de texto ingredientes.txt
+            //AltaMenuPropio(string pChefDoc, string pChefTipoDoc, List<string> pIngredientes, List<int> pCantidades, decimal pGanancia, string pDesc)
+            
+            
         }
         public bool HayDatos()
         {
